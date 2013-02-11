66f959e9186df2b80f736e50b6d34fb8b99c2be2
r0ck5tar <hakim@hakimluqman.com>
2013-02-11 21:33:17 +0100

    Added a comment

--- src/Dummy.java
+++ src/Dummy.java
@@ -7,6 +7,8 @@ public class Dummy {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Looks like I've added new stuff here");
+		
+		//Just one more slight change right here.
 	}
 
 }
